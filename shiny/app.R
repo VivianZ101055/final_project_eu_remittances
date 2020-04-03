440f281b7ca3fdc390378c8bdb09330d5173caba
Vivian Zhang <vivian@Vivians-Air.hitronhub.home>
2020-04-02 23:59:36 -0700

    edits to the labels on shiny app

--- shiny/app.R
+++ shiny/app.R
@@ -41,12 +41,15 @@ server <- function(input, output){
   )
   
   output$distPlot <- renderPlotly({
-    ggplot(mydata, aes(x = year, y = sum_remittance_usd)) +
+    p <- ggplot(mydata, aes(x = year, y = sum_remittance_usd)) +
       geom_line() +
-      geom_point() +
+      geom_point(aes(text = 
+                       paste0("Year: ", year, "\n", 
+                              "Total Remittances: $",
+                              round(sum_remittance_usd/1000000000,0),
+                              "B", sep=""))) +
       labs(title = "Total remittances flowing into the EU, by year",
            # subtitle = "Plotting Remittances in USD, from 1980-2018",
-           # caption = "Source: World Bank",
            x = "Year",
            y = "Total Remittances (in Billions of USD)") +
       scale_x_continuous(limits = c(1980, 2018),
@@ -62,8 +65,12 @@ server <- function(input, output){
                                     100000000000, 110000000000, 120000000000),
                          labels = c("0", "10", "20", "30", "40", "50", "60", "70", "80",
                                     "90", "100", "110", "120")) +
-      theme_classic() %>%
-      add_trace(hovertemplate = "Year: $%{x}", "Total Remittances: $%{y}")
+      theme_classic()
+    
+    # Generates the plot with text hovering feature
+    
+    ggplotly(p, tooltip = "text")
+    
     })
 }
 
