5295cc8c3fb1d64b3892fb89d7808bba10705603
Vivian Zhang <vivian@Vivians-Air.hitronhub.home>
2020-04-03 13:19:56 -0700

    Edits to shinyapp

--- shiny/app.R
+++ shiny/app.R
@@ -2,6 +2,14 @@
 library(shiny)
 library(shinythemes)
 library(plotly)
+library(readxl)
+library(janitor)
+library(rvest)
+library(magrittr)
+library(base)
+library(reshape2)
+library(leaflet)
+library(tidyverse)
 
 #-----------------------------------------------------------------
 #--------------------------Shiny ui-------------------------------
@@ -69,14 +77,13 @@ server <- function(input, output){
   
   output$distPlot <- renderPlotly({
     p <- ggplot(mydata, aes(x = year, y = sum_remittance_usd)) +
-      geom_line() +
+      geom_line(color = "light blue") +
       geom_point(aes(text = 
                        paste0("Year: ", year, "\n", 
                               "Total Remittances: $",
                               round(sum_remittance_usd/1000000000,0),
-                              "B", sep=""))) +
+                              "B", sep="")), color = "dark blue") +
       labs(title = "Total remittances flowing into the EU, by year",
-           # subtitle = "Plotting Remittances in USD, from 1980-2018",
            x = "Year",
            y = "Total Remittances (in Billions of USD)") +
       scale_x_continuous(limits = c(1980, 2018),
@@ -100,41 +107,5 @@ server <- function(input, output){
     
     })
 }
-
-# to enable shiny toolkit, use fluidPage
-
-# ui <- fluidPage(
-#     plotlyOutput("distPlot")
-# )
-
-# defining server for shinyApp function
-
-# server <- function(input, output) {
-#     output$distPlot <- renderPlotly({
-#         ggplot(mydata, aes(x = year, y = sum_remittance_usd)) +
-#             geom_line() +
-#             geom_point() +
-#             labs(title = "Total remittances flowing into the EU, by year",
-#                  # subtitle = "Plotting Remittances in USD, from 1980-2018",
-#                  # caption = "Source: World Bank",
-#                  x = "Year",
-#                  y = "Total Remittances (in Billions of USD)") +
-#             scale_x_continuous(limits = c(1980, 2018),
-#                                breaks = c(1980, 1985, 1990, 1995, 2000, 2005, 2010,
-#                                           2015),
-#                                labels = c("1980", "1985", "1990", "1995",
-#                                           "2000", "2005", "2010",
-#                                           "2015")) +
-#             scale_y_continuous(limits = c(0, 120000000000),
-#                                breaks = c(0, 10000000000, 20000000000, 30000000000,
-#                                           40000000000, 50000000000, 60000000000,
-#                                           70000000000, 80000000000, 90000000000,
-#                                           100000000000, 110000000000, 120000000000),
-#                                labels = c("0", "10", "20", "30", "40", "50", "60", "70", "80",
-#                                           "90", "100", "110", "120")) +
-#             theme_classic() %>%
-#             add_trace(hovertemplate = "Year: $%{x}", "Total Remittances: $%{y}")
-#     })
-# }
 
 shinyApp(ui = ui, server = server)
